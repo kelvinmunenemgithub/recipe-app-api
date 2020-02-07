4eba706c6ba2ce12283d335b3926ea48b372c430
kevonesh <kelvinmunenm@gmail.com>
2020-02-07 16:57:15 +0300

    Added wait for db command

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'z(fe5teaem%!26r6gq-+1c*jevfbu)4o%xm_r!_+co0y=nta3='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -73,8 +73,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
